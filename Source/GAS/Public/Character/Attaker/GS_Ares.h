90fe0a9dddb496a4c3f55353adce09e9e0d23fef
JoongEun Shin <solie0705@gmail.com>
2025-05-08 03:18:17 +0900

    [ ✨ update ] Attaker Class Init
    - GS_Ares GS_Character 추가.
    - GS_Merci GS_Character 추가.
    - GS_Chan GS_Character 추가.

---
+++ Source/GAS/Public/Character/Attaker/GS_Ares.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/GS_Character.h"
+#include "GS_Ares.generated.h"
+
+UCLASS()
+class GAS_API AGS_Ares : public AGS_Character
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AGS_Ares();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
