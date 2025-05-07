dc807d4f3c84cd5cbfa16dae51cdb6bebcd00c19
JoongEun Shin <solie0705@gmail.com>
2025-05-07 20:08:02 +0900

    [ ✨ update ] Base Class Init
    - GS_Character Character 추가.
    - GS_StatComp Actor Component 추가.
    - GS_DebuffComp Actor Component 추가.
    - GS_SkillComp Actor Component 추가.

---
+++ Source/GAS/Public/Character/Component/GS_DebuffComp.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GS_DebuffComp.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GAS_API UGS_DebuffComp : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UGS_DebuffComp();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
