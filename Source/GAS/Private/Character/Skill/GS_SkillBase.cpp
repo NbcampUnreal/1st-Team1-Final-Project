17c8cc971d3af7f75629084134d299d122d2ff2e
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-08 12:23:35 +0900

    [ ✨ update ]  GS_SkillBase 1차 완료
    GS_SkillBase 1차 내부 구현

--- Source/GAS/Private/Character/Skill/GS_SkillBase.cpp
+++ Source/GAS/Private/Character/Skill/GS_SkillBase.cpp
@@ -2,4 +2,45 @@
 
 
 #include "Character/Skill/GS_SkillBase.h"
+#include "Character/GS_Character.h"
 
+float UGS_SkillBase::GetCoolTime()
+{
+	return Cooltime;
+}
+
+void UGS_SkillBase::InitSkill(AGS_Character* InOwner)
+{
+	OwnerCharacter = InOwner;
+}
+
+void UGS_SkillBase::ActiveSkill()
+{
+	if (!CanActive()) return;
+
+	if (SkillAnimMontage)
+	{
+		OwnerCharacter->PlayAnimMontage(SkillAnimMontage);
+	}
+
+	StartCoolDown();
+}
+
+void UGS_SkillBase::ExecuteSkillEffect()
+{
+	// Override
+}
+
+bool UGS_SkillBase::CanActive() const
+{
+	return OwnerCharacter && !bIsCoolingDown;
+}
+
+void UGS_SkillBase::StartCoolDown()
+{
+	bIsCoolingDown = true;
+	OwnerCharacter->GetWorldTimerManager().SetTimer(CooldownHandle, [this]()
+		{
+			bIsCoolingDown = false;
+		}, Cooltime, false);
+}
--- Source/GAS/Public/Character/Skill/GS_SkillBase.h
+++ Source/GAS/Public/Character/Skill/GS_SkillBase.h
@@ -6,6 +6,7 @@
 #include "UObject/NoExportTypes.h"
 #include "GS_SkillBase.generated.h"
 
+class AGS_Character;
 /**
  * 
  */
@@ -13,5 +14,30 @@ UCLASS()
 class GAS_API UGS_SkillBase : public UObject
 {
 	GENERATED_BODY()
-	
+
+public:
+	// 쿨타임 관리
+	float GetCoolTime();
+
+	// 스킬 초기화
+	void InitSkill(AGS_Character* InOwner);
+
+	// 스킬 작동
+	virtual void ActiveSkill(); // 서버 권한에서만 호출
+	virtual void ExecuteSkillEffect();
+	virtual bool CanActive() const;
+
+protected:
+	// 쿨타임 관리
+	FTimerHandle CooldownHandle;
+	float Cooltime;
+	bool bIsCoolingDown;
+	void StartCoolDown();
+
+	// 스킬 공격 애니메이션
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* SkillAnimMontage;
+
+	// 스킬 소유자
+	AGS_Character* OwnerCharacter;
 };
