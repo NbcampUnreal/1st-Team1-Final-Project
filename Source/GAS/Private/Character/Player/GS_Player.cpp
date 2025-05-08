2c54d607694de782bfd49bd0ae11a81656381782
HyungJun <juntrex@naver.com>
2025-05-08 21:45:13 +0900

    [ ✨ update ] GS_Character 및 GS_Player 로직 추가
    * GS_Character : 공격 관련 로직 추가 (확정 아님), 컴포넌트 추가
    * GS_Player : 카메라, 스프링암 컴포넌트 추가, Walk, Run Speed 변수 추가

--- Source/GAS/Private/Character/Player/GS_Player.cpp
+++ Source/GAS/Private/Character/Player/GS_Player.cpp
@@ -1,30 +1,30 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Character/Player/GS_Player.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
-// Sets default values
 AGS_Player::AGS_Player()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComp->TargetArmLength = 400.f;
+	SpringArmComp->bUsePawnControlRotation = true;
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComp->bUsePawnControlRotation = false;
+	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
 }
 
-// Called when the game starts or when spawned
 void AGS_Player::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
-void AGS_Player::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
 
-// Called to bind functionality to input
 void AGS_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
