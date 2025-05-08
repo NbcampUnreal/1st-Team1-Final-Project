7c8b641b114aad20ebaf15e76e45f89d1aa646cb
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-08 23:01:51 +0900

    [ ✨ update ]  ChanAimingSkill 구현 중

--- Source/GAS/Private/Character/Skill/Seeker/GS_SeekerSkillBase.cpp
+++ Source/GAS/Private/Character/Skill/Seeker/GS_SeekerSkillBase.cpp
@@ -8,11 +8,11 @@ void UGS_SeekerSkillBase::ActiveSkill()
 	Super::ActiveSkill();
 }
 
-void UGS_SeekerSkillBase::ApplyEffectToBoss()
+void UGS_SeekerSkillBase::ApplyEffectToBoss(AActor* Target)
 {
 }
 
-void UGS_SeekerSkillBase::ApplyEffectToDungeonMonster()
+void UGS_SeekerSkillBase::ApplyEffectToDungeonMonster(AActor* Target)
 {
 }
 
