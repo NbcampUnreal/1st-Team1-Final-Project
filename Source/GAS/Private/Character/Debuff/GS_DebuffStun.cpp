92d043f0943d17a3a4547b8a47b1445e40c29223
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-09 15:48:47 +0900

    [ ✨ update ]  DebuffStun 구현 중

---
+++ Source/GAS/Private/Character/Debuff/GS_DebuffStun.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Debuff/GS_DebuffStun.h"
+
+void UGS_DebuffStun::OnApply()
+{
+	if (TargetCharacter)
+	{
+
+	}
+}
+
+void UGS_DebuffStun::OnExpire()
+{
+	if (TargetCharacter)
+	{
+
+	}
+}
