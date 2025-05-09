dc39973f77b71fd86b592f522822c4f637a442f8
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-09 15:48:26 +0900

    [ ✨ update ]  DebuffBase 구현

---
+++ Source/GAS/Private/Character/Debuff/GS_DebuffBase.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Debuff/GS_DebuffBase.h"
+
+void UGS_DebuffBase::Initialize(AGS_Character* InTarget, float InDuration, int32 InPriority)
+{
+	TargetCharacter = InTarget;
+	Duration = InDuration;
+	Priority = InPriority;
+}
+
+void UGS_DebuffBase::OnApply()
+{
+}
+
+void UGS_DebuffBase::OnExpire()
+{
+}
---
+++ Source/GAS/Public/Character/Debuff/GS_DebuffBase.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GS_DebuffBase.generated.h"
+
+class AGS_Character;
+/**
+ * 
+ */
+UCLASS()
+class GAS_API UGS_DebuffBase : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(AGS_Character* InTarget, float InDuration, int32 InPriority);
+	virtual void OnApply();
+	virtual void OnExpire();
+	
+	int32 GetPriority() const { return Priority; }
+	float GetDuration() const { return Duration; }
+
+protected:
+	UPROPERTY()
+	AGS_Character* TargetCharacter;
+	float Duration;
+	int32 Priority;
+};
