ef468d1551cb435182b746f9aef018ee4f0f29d0
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-10 19:43:41 +0900

    [ ✨ update ]  DebuffStun 경직 디버프 구현

--- Source/GAS/Private/Character/Debuff/GS_DebuffStun.cpp
+++ Source/GAS/Private/Character/Debuff/GS_DebuffStun.cpp
@@ -2,15 +2,25 @@
 
 
 #include "Character/Debuff/GS_DebuffStun.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Character/GS_Character.h"
+#include "Character/Skill/GS_SkillComp.h"
 
 void UGS_DebuffStun::OnApply()
 {
+	Super::OnApply();
+
 	if (TargetCharacter)
 	{
-		// 일반 공격만
 		// 움직임도 멈춤
+		TargetCharacter->GetCharacterMovement()->DisableMovement();
+		
 		// 스킬 못쓰고
 		// 스킬을 끊지는 않음
+		if (UGS_SkillComp* SkillComp = TargetCharacter->FindComponentByClass<UGS_SkillComp>())
+		{
+			SkillComp->SetCanUseSkill(false);
+		}
 	}
 }
 
@@ -18,6 +28,15 @@ void UGS_DebuffStun::OnExpire()
 {
 	if (TargetCharacter)
 	{
+		// 움직임
+		TargetCharacter->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
 
+		// 스킬 사용 가능
+		if (UGS_SkillComp* SkillComp = TargetCharacter->FindComponentByClass<UGS_SkillComp>())
+		{
+			SkillComp->SetCanUseSkill(true);
+		}
 	}
+
+	Super::OnExpire();
 }
