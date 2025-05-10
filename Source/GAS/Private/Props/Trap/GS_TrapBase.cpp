1b0e2c690f074a6ce1a7900444618a321d271ae1
Noa <tabacal3590@gmail.com>
2025-05-11 01:12:37 +0900

    [ ✨ update ]   함정 5가지 동작 구현 1차 완료 및 폴더 위치 변경
    - 콘텐츠 브라우저 상 트랩 파일 위치 조정
    - KimYJ_TestLevel : level 이름 혼동될까봐 KimYJ로 변경
    - GS_TrapBase / GS_TrigTrapBase : 컴포넌트 구조 조정(Rotatiton Scene 추가)

--- Source/GAS/Private/Props/Trap/GS_TrapBase.cpp
+++ Source/GAS/Private/Props/Trap/GS_TrapBase.cpp
@@ -7,45 +7,35 @@ AGS_TrapBase::AGS_TrapBase()
 	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
 	RootComponent = RootScene;
 
+	RotationScene = CreateDefaultSubobject<USceneComponent>(TEXT("RotationScene"));
+	RotationScene->SetupAttachment(RootComponent);
+
 	TrapStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TrapStaticMesh"));
-	TrapStaticMesh->SetupAttachment(RootComponent);
+	TrapStaticMesh->SetupAttachment(RotationScene);
 
 	DamageBox = CreateDefaultSubobject<UBoxComponent>(TEXT("DamageBox"));
-	DamageBox->SetupAttachment(RootComponent);
+	DamageBox->SetupAttachment(TrapStaticMesh);
 	//needs to be fixed
-	DamageBox->SetCollisionProfileName(TEXT("OverlapAll"));
+	// DamageBox 콜리전 설정
+	DamageBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	DamageBox->SetCollisionObjectType(ECC_Pawn); // Object Type을 Pawn으로 설정
+
+	// 기본 응답: 모두 무시
+	DamageBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+
+	// 트레이스 채널
+	DamageBox->SetCollisionResponseToChannel(ECC_Visibility, ECR_Overlap);
+	DamageBox->SetCollisionResponseToChannel(ECC_Camera, ECR_Overlap);
+
+	// 오브젝트 채널
+	DamageBox->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	DamageBox->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	DamageBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	DamageBox->SetCollisionResponseToChannel(ECC_PhysicsBody, ECR_Ignore);
+	DamageBox->SetCollisionResponseToChannel(ECC_Vehicle, ECR_Block);
+	DamageBox->SetCollisionResponseToChannel(ECC_Destructible, ECR_Overlap);
 }
 
-////load static mesh on Construnction not begin play
-//void AGS_TrapBase::OnConstruction(const FTransform& Transform)
-//{
-//	Super::OnConstruction(Transform);
-//
-//	if (!TrapDataTable || TrapID.IsNone()) return;
-//	FTrapData* FoundData = TrapDataTable->FindRow<FTrapData>(TrapID, TEXT("Trap Lookup"));
-//	
-//
-//	if (FoundData)
-//	{
-//		
-//		TrapData = *FoundData;
-//		if (TrapData.TrapMesh.IsValid())
-//		{
-//			UE_LOG(LogTemp, Warning, TEXT("TrapData found! Mesh is valid? %s"), TrapData.TrapMesh.IsValid() ? TEXT("Yes") : TEXT("No"));
-//			TrapStaticMesh->SetStaticMesh(TrapData.TrapMesh.LoadSynchronous());
-//		}
-//		else
-//		{
-//			UE_LOG(LogTemp, Warning, TEXT("TrapData found!but not valid"));
-//		}
-//	}
-//	else
-//	{
-//
-//		UE_LOG(LogTemp, Warning, TEXT("Trap with ID '%s' not found in DataTable"), *TrapID.ToString());
-//	}
-//
-//}
 
 void AGS_TrapBase::BeginPlay()
 {
@@ -59,8 +49,10 @@ void AGS_TrapBase::OnDamageBoxOverlap(UPrimitiveComponent* OverlappedComp, AActo
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 	bool bFromSweep, const FHitResult& SweepResult)
 {
+	UE_LOG(LogTemp, Warning, TEXT("DamageBox 1"));
 	if (OtherActor && OtherActor != this)
 	{
+		UE_LOG(LogTemp, Warning, TEXT("DamageBox 2"));
 		DamageBoxEffect(OtherActor);
 		HandleTrapDamage(OtherActor);
 	}
