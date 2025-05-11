a31146b3dfc1b6b226e8b47ea673b9638e91bce2
yujin1025 <kyjc011006@gmail.com>
2025-05-11 21:41:44 +0900

    [ ✨ update ] rts 카메라 이동 구현 (마우스, 키보드 입력)
    마우스, 키보드 입력 동시에 받을 경우 키보드 입력 1순위로

--- Source/GAS/Public/AI/RTS/GS_RTSController.h
+++ Source/GAS/Public/AI/RTS/GS_RTSController.h
@@ -6,12 +6,50 @@
 #include "GameFramework/PlayerController.h"
 #include "GS_RTSController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class GAS_API AGS_RTSController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AGS_RTSController();
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputAction* CameraMoveAction;
+
+	UFUNCTION()
+	void CameraMove(const FInputActionValue& value);
+	
+	UFUNCTION()
+	void CameraMoveEnd();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	FVector2D KeyboardDir = FVector2D::ZeroVector;
+	FVector2D MouseEdgeDir = FVector2D::ZeroVector;
+
+	UPROPERTY()
+	class AGS_RTSCamera* CameraActor = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	float CameraSpeed = 2000.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Camera", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	float EdgeScreenRatio = 0.05f;
+
+	FVector2D GetKeyboardDirection() const;
+	FVector2D GetMouseEdgeDirection() const;
+	FVector2D GetFinalDirection() const;
+	void MoveCamera(const FVector2D& Direction, float DeltaTime);
+	void InitCameraActor();
 };
