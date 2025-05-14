dd60a64e43bb41dd3edc86c7e5001c5b1efa6fd6
sehee-jj <seheesay@gmail.com>
2025-05-14 19:40:19 +0900

    [ ✨ update ]  세이브게임 클래스 추가

---
+++ Source/GAS/Public/RuneSystem/GS_ArcaneBoardSaveGame.h
@@ -0,0 +1,24 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "GS_ArcaneBoardTypes.h"
+#include "GS_ArcaneBoardSaveGame.generated.h"
+
+/**
+ * 아케인 보드 시스템의 로컬 플레이어별 저장 데이터
+ */
+UCLASS()
+class GAS_API UGS_ArcaneBoardSaveGame : public ULocalPlayerSaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TMap<ECharacterClass, FRunePlacementData> SavedRunesByClass;
+
+	UPROPERTY()
+	FCharacterStats AppliedStatEffects;
+};
