43df02d04be8680e826e6e9c0dcaed6ef81e0fa2
sehee-jj <seheesay@gmail.com>
2025-05-16 12:07:49 +0900

    [ ✨ update ]  위젯 C++ 클래스 생성

---
+++ Source/GAS/Public/UI/RuneSystem/GS_RuneGridCellWidget.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GS_RuneGridCellWidget.generated.h"
+
+
+/**
+ * 룬 그리드 셀 위젯
+ */
+UCLASS()
+class GAS_API UGS_RuneGridCellWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
---
+++ Source/GAS/Public/UI/RuneSystem/GS_RuneInventoryWidget.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GS_RuneInventoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API UGS_RuneInventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
