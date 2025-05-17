ec2a81161bb7bb4b4211b72715b6300157ac8c72
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-17 15:21:49 +0900

    [ 🩹 fix ] GS_DebuffComp 주석 제거

--- Source/GAS/Private/Character/Component/GS_DebuffComp.cpp
+++ Source/GAS/Private/Character/Component/GS_DebuffComp.cpp
@@ -8,11 +8,8 @@
 #include "Net/UnrealNetwork.h"
 
 
-// Sets default values for this component's properties
 UGS_DebuffComp::UGS_DebuffComp()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
 
 	SetIsReplicatedByDefault(true);
