d2d84d0e985a63c7236ca69d524d9f9227fd43b1
Noa <tabacal3590@gmail.com>
2025-05-19 00:22:15 +0900

    [ ✨ update ]  화살 발사기 전용 화살 구현 완료
    GS_ArrowTrapProjectile : 화살 발사기의 화살 추가(플레이어에게 데미지 입히는 것까지 완료)
    BP_ArrowTrapArrow : 화살 발사기의 화살 Bp 생성 완료(발사체 속도 3000으로 설정)
    BP_ArrowTrap : GS_ArrowTrapProjectile의 Init 함수 노드 연결

--- Source/GAS/Private/Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.cpp
+++ Source/GAS/Private/Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.cpp
@@ -1,2 +1,32 @@
 #include "Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.h"
+#include "Components/SphereComponent.h"
+#include "Character/Player/GS_Player.h"
 
+
+AGS_ArrowTrapProjectile::AGS_ArrowTrapProjectile()
+{
+
+}
+
+
+void AGS_ArrowTrapProjectile::Init(AGS_TrigTrapBase* InTrap)
+{
+	OwningTrap = InTrap;
+	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &AGS_ArrowTrapProjectile::OnBeginOverlap);
+}
+
+
+void AGS_ArrowTrapProjectile::OnBeginOverlap(
+	UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor || OtherActor == this) return;
+
+	AGS_Player* Player = Cast<AGS_Player>(OtherActor);
+	if (Player && OwningTrap)
+	{
+		OwningTrap->HandleTrapDamage(OtherActor);
+		StickWithVisualOnly(SweepResult);
+	}
+}
--- Source/GAS/Public/Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.h
+++ Source/GAS/Public/Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.h
@@ -2,11 +2,30 @@
 
 #include "CoreMinimal.h"
 #include "Weapon/Projectile/GS_WeaponProjectile.h"
+#include "Weapon/Projectile/Seeker/GS_SeekerMerciArrow.h"
+#include "Props/Trap/GS_TrapBase.h"
+#include "Props/Trap/TriggerTrap/GS_TrigTrapBase.h"
+#include "Weapon/Projectile/Seeker/GS_SeekerMerciArrowNormal.h"
 #include "GS_ArrowTrapProjectile.generated.h"
 
+
 UCLASS()
-class GAS_API AGS_ArrowTrapProjectile : public AGS_WeaponProjectile
+class GAS_API AGS_ArrowTrapProjectile : public AGS_SeekerMerciArrow
 {
 	GENERATED_BODY()
-	
+
+public:
+	AGS_ArrowTrapProjectile();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Trap")
+	AGS_TrigTrapBase* OwningTrap;
+
+	UFUNCTION()
+	void OnBeginOverlap(
+		UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintCallable, Category = "Trap")
+	void Init(AGS_TrigTrapBase* InTrap);
 };
