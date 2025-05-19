8206e7809ca6a580c8130bd01a6cca9b43e8b196
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-19 17:24:21 +0900

    [ 🩹 fix ] Debuff Todo 메모 전달

--- Source/GAS/Private/Character/Debuff/GS_DebuffAggro.cpp
+++ Source/GAS/Private/Character/Debuff/GS_DebuffAggro.cpp
@@ -16,14 +16,23 @@ void UGS_DebuffAggro::OnApply()
 	{
 		// 타겟 바꾸기
 		AGS_AIController* AIController = Cast<AGS_AIController>(TargetCharacter->GetController());
-		if (!AIController) return;
+		if (!AIController) 
+		{
+			return;
+		}
 
 		UBlackboardComponent* BB = AIController->GetBlackboardComponent();
-		if (!BB) return;
+		if (!BB) 
+		{
+			return;
+		}
 
 		BB->SetValueAsObject(AGS_AIController::TargetActorKey, OwnerCharacter);
 
+		// Todo: Target이 바뀌지 않도록 Lock 처리할 수 있을까요?
+
 		// 조종 불가
+		// Todo: 몬스터를 선택할 수 있습니다. 하지만 원하는 곳으로 이동시키는 등 명령을 내릴 수 없습니다.
 	}
 }
 
@@ -31,7 +40,9 @@ void UGS_DebuffAggro::OnExpire()
 {
 	if (TargetCharacter)
 	{
-		// 타겟 없애기
+		// Todo: Target이 바뀌지 않도록 Lock 처리 해제
+		
+		// 타겟 없애기 => 안해도 될 것 같음(그냥 그대로 방패병으로 두고 Lock만 해제)
 		AGS_AIController* AIController = Cast<AGS_AIController>(TargetCharacter->GetController());
 		if (AIController)
 		{
@@ -39,11 +50,12 @@ void UGS_DebuffAggro::OnExpire()
 			if (BB)
 			{
 				// 강제로 설정했던 TargetActor 제거
-				BB->ClearValue(AGS_AIController::TargetActorKey);
+				//BB->ClearValue(AGS_AIController::TargetActorKey);
 			}
 		}
 
 		// 조종 불가
+		// Todo: 몬스터를 선택할 수 있으며 이제 원하는 곳으로 이동시키는 등 명령을 내릴 수 있습니다.
 	}
 
 	Super::OnExpire();
