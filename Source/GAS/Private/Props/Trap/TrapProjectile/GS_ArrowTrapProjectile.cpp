cafb7c7b23aadd24da4995ef6c8ee978ec85c6f0
Noa <tabacal3590@gmail.com>
2025-05-20 20:46:23 +0900

    [ 🩹 fix ] 함정 화살 발사기의 화살 구조 수정
    GS_ArrowTrapProjectile : 화살 발사기의 화살 구조 수정

--- Source/GAS/Private/Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.cpp
+++ Source/GAS/Private/Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.cpp
@@ -5,14 +5,20 @@
 
 AGS_ArrowTrapProjectile::AGS_ArrowTrapProjectile()
 {
-
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> ArrowMeshObj(TEXT("/Game/Weapons/Bow_02_Quiver/SKM_Arrow_02.SKM_Arrow_02"));
+	if (ArrowMeshObj.Succeeded())
+	{
+		ProjectileMesh->SetSkeletalMesh(ArrowMeshObj.Object);
+	}
 }
-
 
 void AGS_ArrowTrapProjectile::Init(AGS_TrigTrapBase* InTrap)
 {
 	OwningTrap = InTrap;
-	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &AGS_ArrowTrapProjectile::OnBeginOverlap);
+	if (HasAuthority())
+	{
+		CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &AGS_ArrowTrapProjectile::OnBeginOverlap);
+	}
 }
 
 
@@ -27,6 +33,54 @@ void AGS_ArrowTrapProjectile::OnBeginOverlap(
 	if (Player && OwningTrap)
 	{
 		OwningTrap->HandleTrapDamage(OtherActor);
-		StickWithVisualOnly(SweepResult);
 	}
+	StickWithVisualOnly(SweepResult);
+}
+
+void AGS_ArrowTrapProjectile::StickWithVisualOnly(const FHitResult& Hit)
+{
+	if (!ProjectileMesh)
+	{
+		return;
+	}
+
+	// 화살이 박힌 위치
+	FVector SpawnLocation = Hit.ImpactPoint;
+
+	// 방향 = 화살이 실제 날아온 방향
+	FVector ArrowDirection = GetVelocity().GetSafeNormal();
+	FRotator SpawnRotation = FRotationMatrix::MakeFromX(ArrowDirection).Rotator();
+
+	// 메시가 +Y 방향을 앞이라고 가정 시 Y축 기준 -90도 회전
+	FRotator AdjustedRotation = SpawnRotation + FRotator(0.f, -90.f, 0.f);
+	SpawnLocation -= ArrowDirection * 25.f; // 약간 파고들게
+
+	FActorSpawnParameters Params;
+	Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	
+	TSubclassOf<AGS_ArrowVisualActor> VisualArrowClass = AGS_ArrowVisualActor::StaticClass();
+
+	if (VisualArrowClass)
+	{
+		AGS_ArrowVisualActor* VisualArrow = GetWorld()->SpawnActor<AGS_ArrowVisualActor>(VisualArrowClass, SpawnLocation, AdjustedRotation, Params);
+		UE_LOG(LogTemp, Warning, TEXT("VisualArrowClass exist"));
+		if (VisualArrow)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("VisualArrow exist"));
+			VisualArrow->SetArrowMesh(ProjectileMesh->GetSkeletalMeshAsset());
+			VisualArrow->AttachToComponent(Hit.GetComponent(), FAttachmentTransformRules::KeepWorldTransform);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("No VisualArrow"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No VisualArrowClass"));
+	}
+	
+
+	// 본 화살 제거
+	Destroy();
 }
--- Source/GAS/Public/Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.h
+++ Source/GAS/Public/Props/Trap/TrapProjectile/GS_ArrowTrapProjectile.h
@@ -5,20 +5,27 @@
 #include "Weapon/Projectile/Seeker/GS_SeekerMerciArrow.h"
 #include "Props/Trap/GS_TrapBase.h"
 #include "Props/Trap/TriggerTrap/GS_TrigTrapBase.h"
+#include "Weapon/Projectile/Seeker/GS_ArrowVisualActor.h"
 #include "Weapon/Projectile/Seeker/GS_SeekerMerciArrowNormal.h"
 #include "GS_ArrowTrapProjectile.generated.h"
 
 
 UCLASS()
-class GAS_API AGS_ArrowTrapProjectile : public AGS_SeekerMerciArrow
+class GAS_API AGS_ArrowTrapProjectile : public AGS_WeaponProjectile
 {
 	GENERATED_BODY()
 
 public:
 	AGS_ArrowTrapProjectile();
 
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* ArrowMesh;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Trap")
 	AGS_TrigTrapBase* OwningTrap;
+
+	UFUNCTION(BlueprintCallable, Category = "Trap")
+	void Init(AGS_TrigTrapBase* InTrap);
 
 	UFUNCTION()
 	void OnBeginOverlap(
@@ -26,6 +33,9 @@ public:
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 		bool bFromSweep, const FHitResult& SweepResult);
 
-	UFUNCTION(BlueprintCallable, Category = "Trap")
-	void Init(AGS_TrigTrapBase* InTrap);
+	void StickWithVisualOnly(const FHitResult& Hit);
+
+	
+
+
 };
