5663d0d113cdc24cfc08b2dbf50d678cbf1736ce
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-26 10:54:05 +0900

    [ 🐞 bugfix ] ArrowVisualActor Mesh 동기화 오류 수정

--- Source/GAS/Private/Weapon/Projectile/Seeker/GS_ArrowVisualActor.cpp
+++ Source/GAS/Private/Weapon/Projectile/Seeker/GS_ArrowVisualActor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Weapon/Projectile/Seeker/GS_ArrowVisualActor.h"
+#include "Net/UnrealNetwork.h"
 
 
 AGS_ArrowVisualActor::AGS_ArrowVisualActor()
@@ -10,11 +11,19 @@ AGS_ArrowVisualActor::AGS_ArrowVisualActor()
 	bReplicates = true;
 	SetReplicateMovement(true);
 
+
+
 	ArrowMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ArrowMesh"));
+	ArrowMesh->SetIsReplicated(true);
 	SetRootComponent(ArrowMesh);
 
 	ArrowMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	ArrowMesh->SetSimulatePhysics(false);
+}
+
+void AGS_ArrowVisualActor::OnRep_SkeletalMesh()
+{
+		ArrowMesh->SetSkeletalMesh(CurrentMesh);
 }
 
 void AGS_ArrowVisualActor::SetArrowMesh(USkeletalMesh* Mesh)
@@ -22,6 +31,19 @@ void AGS_ArrowVisualActor::SetArrowMesh(USkeletalMesh* Mesh)
 	if (ArrowMesh && Mesh)
 	{
 		ArrowMesh->SetSkeletalMesh(Mesh);
+		CurrentMesh = Mesh;
+		UE_LOG(LogTemp, Warning, TEXT("Arrow Mesh Set!!!!!!!!!!"));
 	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Arrow Mesh null or Mesh null"));
+	}
+}
+
+void AGS_ArrowVisualActor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AGS_ArrowVisualActor, CurrentMesh);
 }
 
