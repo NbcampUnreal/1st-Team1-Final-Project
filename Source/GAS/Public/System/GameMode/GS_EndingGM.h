bc40acfa1cfea56d5d4f867f32aea3ef6cbe63ff
IINGS <wjdgns10293@gmail.com>
2025-05-27 16:13:05 +0900

    [ ✨ update ] 보스 레벨과 엔딩 레벨에 쓸 GM,GS 생성

---
+++ Source/GAS/Public/System/GameMode/GS_EndingGM.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GS_EndingGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API AGS_EndingGM : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
