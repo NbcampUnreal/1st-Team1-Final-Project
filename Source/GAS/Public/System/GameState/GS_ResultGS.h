977ca8804006838b030444303d309c98a1371022
IINGS <wjdgns10293@gmail.com>
2025-05-28 01:52:12 +0900

    [ 🔥 rename ] 파일 이름 변경
    GS_Ending -> GS_Result

---
+++ Source/GAS/Public/System/GameState/GS_ResultGS.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GS_ResultGS.generated.h"
+
+
+UCLASS()
+class GAS_API AGS_ResultGS : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
