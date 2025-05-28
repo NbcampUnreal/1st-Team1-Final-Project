9519f557daf692f7d50571c52389127cac84a501
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-05-28 14:47:34 +0900

    [ ✨ update ] Skill Montage 재생 기능 구현중

--- Source/GAS/Private/Character/Skill/Seeker/Chan/GS_ChanUltimateSkill.cpp
+++ Source/GAS/Private/Character/Skill/Seeker/Chan/GS_ChanUltimateSkill.cpp
@@ -1,5 +1,17 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Character/Skill/Seeker/Chan/GS_ChanUltimateSkill.h"
+#include "Character/Player/GS_Player.h"
 
+void UGS_ChanUltimateSkill::ActiveSkill()
+{
+	if (!CanActive()) return;
+	Super::ActiveSkill();
+	AGS_Player* OwnerPlayer = Cast<AGS_Player>(OwnerCharacter);
+	OwnerPlayer->Multicast_PlaySkillMontage(SkillAnimMontages[0]);
+}
+
+void UGS_ChanUltimateSkill::ExecuteSkillEffect()
+{
+}
