c291efcdff62208226129191b12b7276e2315cf2
HyungJun <juntrex@naver.com>
2025-05-28 09:48:22 +0900

    [ ✨ update ] 스킬 데미지 추가
    * DT_SkillSet을 통해 스킬 데미지를 넣을 수 있도록 기능 추가
    * 수정된 스킬 DT에 맞게 가디언(Drakhar) 데미지 구조 추가
    + Drakhar ComboAttack 몽타주 애니메이션 수정

--- Source/GAS/Private/Character/Player/Guardian/GS_DrakharAnimInstance.cpp
+++ Source/GAS/Private/Character/Player/Guardian/GS_DrakharAnimInstance.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Character/Player/Guardian/GS_DrakharAnimInstance.h"
 #include "Character/Player/Guardian/GS_Drakhar.h"
 
