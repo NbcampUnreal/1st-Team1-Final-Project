3db36703fd0ff6075c36d93e982d11e47d3cfdbc
Noa <tabacal3590@gmail.com>
2025-06-03 22:57:07 +0900

    [ ✨ update ]  결과창 위젯 생성
    결과창 위젯 생성 및 작성중(dev 받아오기 위함)

---
+++ Source/GAS/Public/UI/Screen/Result/GS_Result_GuardianUI.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GS_Result_GuardianUI.generated.h"
+
+
+UCLASS()
+class GAS_API UGS_Result_GuardianUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
