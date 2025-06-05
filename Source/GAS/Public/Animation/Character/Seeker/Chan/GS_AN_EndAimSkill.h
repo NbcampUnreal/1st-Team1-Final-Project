c9ebd04acf853ebe84f602d0a10190e18e69de93
JoongEun Shin <solie0705@gmail.com>
2025-06-05 14:51:26 +0900

    [ 🐞 bugfix ] Chan 스킬 문제 수정.
    - Chan Aim Skill -> Combo Attack 안되는 문제 해결
    - Chan Move Skill -> Combo Attack  안되는 문제 해결
    - 스킬 사용 중 다른 키 입력 되는 문제 해결

---
+++ Source/GAS/Public/Animation/Character/Seeker/Chan/GS_AN_EndAimSkill.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "GS_AN_EndAimSkill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API UGS_AN_EndAimSkill : public UAnimNotify
+{
+	GENERATED_BODY()
+};
