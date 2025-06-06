b1078cc3c14b7a3c719bcd41754e2b88ebe4c404
raindrovvv <vnrs97@gmail.com>
2025-06-06 15:55:47 +0900

    [ ✨ update ] Chan 히트 이펙트 구현(근접 무기)

--- Source/GAS/Public/Weapon/Equipable/GS_WeaponAxe.h
+++ Source/GAS/Public/Weapon/Equipable/GS_WeaponAxe.h
@@ -4,7 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GS_WeaponEquipable.h"
+#include "Engine/HitResult.h"
+#include "AkAudioEvent.h"
+#include "NiagaraSystem.h"
 #include "GS_WeaponAxe.generated.h"
+
+UENUM(BlueprintType)
+enum class EAxeHitTargetType : uint8
+{
+	Guardian		UMETA(DisplayName = "Guardian"),
+	DungeonMonster	UMETA(DisplayName = "DungeonMonster"),
+	Seeker			UMETA(DisplayName = "Seeker"),
+	Structure		UMETA(DisplayName = "Structure"),
+	Other			UMETA(DisplayName = "Other")
+};
 
 UCLASS()
 class GAS_API AGS_WeaponAxe : public AGS_WeaponEquipable
@@ -41,12 +54,25 @@ public:
 	UFUNCTION(Server, Reliable)
 	void ServerDisableHit();
 
-	// 히트 사운드
-	//UPROPERTY(EditDefaultsOnly, Category = "Sound")
-	//UAkAudioEvent* HitSound;
+	// 히트 사운드 에셋들
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Audio")
+	UAkAudioEvent* HitPawnSoundEvent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Audio")
+	UAkAudioEvent* HitStructureSoundEvent;
+
+	// 히트 VFX 에셋들
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "VFX")
+	UNiagaraSystem* HitPawnVFX;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "VFX")
+	UNiagaraSystem* HitStructureVFX;
 
 	UPROPERTY()
 	class AGS_Character* OwnerChar;
+
+	UPROPERTY()
+	TSet<AActor*> HitActors;
 
 protected:
 	// Called when the game starts or when spawned
@@ -55,6 +81,20 @@ protected:
 	UPROPERTY(VisibleAnywhere, Category = "Attack")
 	class UBoxComponent* HitBox;
 
+	// 히트 이펙트 관련 함수들
+	virtual EAxeHitTargetType DetermineTargetType(AActor* OtherActor) const;
+	virtual void PlayHitSound(EAxeHitTargetType TargetType, const FHitResult& SweepResult);
+	virtual void PlayHitVFX(EAxeHitTargetType TargetType, const FHitResult& SweepResult);
 
+	// 멀티캐스트 함수들
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_PlayHitSound(EAxeHitTargetType TargetType, const FHitResult& SweepResult);
+	bool Multicast_PlayHitSound_Validate(EAxeHitTargetType TargetType, const FHitResult& SweepResult);
+	void Multicast_PlayHitSound_Implementation(EAxeHitTargetType TargetType, const FHitResult& SweepResult);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_PlayHitVFX(EAxeHitTargetType TargetType, const FHitResult& SweepResult);
+	bool Multicast_PlayHitVFX_Validate(EAxeHitTargetType TargetType, const FHitResult& SweepResult);
+	void Multicast_PlayHitVFX_Implementation(EAxeHitTargetType TargetType, const FHitResult& SweepResult);
 };
 
