c5816346531ac95bd1d79450292a78dacb64dba3
IINGS <wjdgns10293@gmail.com>
2025-06-12 23:43:44 +0900

    [ ✨ update ] 스팀 친구 초대 기능 구현
    CustomLobbyUI:
    친구 리스트 보여줄 overlay 추가
    FriendListWidgetClass 바인딩
    
    FriendListEntryWidget:
    스팀 친구 항목 구성
    invite 버튼을 누르면 브로드캐스트
    
    FriendListWidget:
    친구 항목들 모아둔 위젯.
    새로고침, 닫기, 초대 버튼 기능 구현

--- Source/GAS/Public/UI/Popup/GS_FriendListWidget.h
+++ Source/GAS/Public/UI/Popup/GS_FriendListWidget.h
@@ -1,52 +1,52 @@
-//#pragma once
-//
-//#include "CoreMinimal.h"
-//#include "Blueprint/UserWidget.h"
-//#include "Interfaces/OnlineFriendsInterface.h"
-//#include "GS_FriendListWidget.generated.h"
-//
-//class UScrollBox;
-//class UGS_FriendListEntryWidget;
-//class UButton;
-//class UCircularThrobber; // 로딩 표시용
-//
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnFriendInviteRequestedDelegate, FUniqueNetIdPtr, FriendIdToInvite);
-//DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnRefreshFriendsClickedDelegate);
-//
-//UCLASS()
-//class GAS_API UGS_FriendListWidget : public UUserWidget
-//{
-//	GENERATED_BODY()
-//	
-//public:
-//	UPROPERTY(meta = (BindWidget))
-//	UScrollBox* FriendsScrollBox;
-//	UPROPERTY(meta = (BindWidget))
-//	UButton* RefreshButton;
-//	UPROPERTY(meta = (BindWidget))
-//	UButton* CloseButton;
-//	UPROPERTY(meta = (BindWidget))
-//	UCircularThrobber* LoadingIndicator;
-//
-//	void FillFriendList(const TArray<TSharedRef<FOnlineFriend>>& Friends);
-//	void ClearFriendList();
-//
-//	UPROPERTY(BlueprintAssignable, Category = "Friends|Event")
-//	FOnFriendInviteRequestedDelegate OnFriendInviteRequested;
-//	UPROPERTY(BlueprintAssignable, Category = "Friends|Event")
-//	FOnRefreshFriendsCickedDelegate OnRefreshFriendsClicked;
-//	
-//protected:
-//	virtual void NativeConstruct() override;
-//	virtual void NativeDestruct() override;
-//
-//	TSubclassOf<UGS_FriendListEntryWidget> FriendListEntryWidgetClass;
-//
-//private:
-//	UFUNCTION()
-//	void HandleFriendInviteButtonClicked(FUniqueNetIdPtr FriendId);
-//	UFUNCTION()
-//	void HandleRefreshButtonClicked();
-//	UFUNCTION()
-//	void HandleCloseButtonClicked();
-//};
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OnlineSubsystemTypes.h" // FUniqueNetIdRepl
+#include "Interfaces/OnlineFriendsInterface.h"
+#include "GS_FriendListWidget.generated.h"
+
+class UScrollBox;
+class UGS_FriendListEntryWidget;
+class UButton;
+class UCircularThrobber;
+
+UCLASS()
+class GAS_API UGS_FriendListWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    void FillFriendList(const TArray<TSharedRef<FOnlineFriend>>& Friends);
+    void ClearFriendList();
+    void ShowLoadingIndicator();
+
+
+protected:
+    virtual void NativeConstruct() override;
+
+    // 위젯 바인딩
+    UPROPERTY(meta = (BindWidget))
+    UScrollBox* FriendsScrollBox;
+    UPROPERTY(meta = (BindWidget))
+    UButton* RefreshButton;
+    UPROPERTY(meta = (BindWidget))
+    UButton* CloseButton;
+    UPROPERTY(meta = (BindWidget))
+    UCircularThrobber* LoadingIndicator;
+
+    UPROPERTY(EditDefaultsOnly, Category = "UI")
+    TSubclassOf<UGS_FriendListEntryWidget> FriendListEntryWidgetClass;
+
+private:
+    UFUNCTION()
+    void HandleFriendInviteButtonClicked(UGS_FriendListEntryWidget* ClickedEntry);
+
+    UFUNCTION()
+    void HandleRefreshButtonClicked();
+
+    UFUNCTION()
+    void HandleCloseButtonClicked();
+
+    void OnReadFriendsListComplete(int32 LocalUserNum, bool bWasSuccessful, const FString& ListName, const FString& ErrorStr);
+};
