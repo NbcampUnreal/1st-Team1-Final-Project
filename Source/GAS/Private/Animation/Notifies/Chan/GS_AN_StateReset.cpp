5b66ed4fea600a308567660372eefaf1347ddf4c
raindrovvv <vnrs97@gmail.com>
2025-06-21 01:49:47 +0900

    [ ✨ update ] StateReset 업로드
    * 수정하지 않았는데 왜 갑자기 보이는지는 모르겠음.

---
+++ Source/GAS/Private/Animation/Notifies/Chan/GS_AN_StateReset.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/Notifies/Chan/GS_AN_StateReset.h"
+#include "Character/Player/Seeker/GS_Seeker.h"
+
+void UGS_AN_StateReset::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (AGS_Seeker* Character = Cast<AGS_Seeker>(MeshComp->GetOwner()))
+	{
+		if (Character->HasAuthority())
+		{
+			Character->bComboEnded = true;
+			Character->Multicast_SetIsFullBodySlot(false);
+			Character->Multicast_SetIsUpperBodySlot(false);
+			Character->Multicast_SetMustTurnInPlace(false);
+			Character->Server_SetMoveControlValue(true, true);
+			Character->Server_SetLookControlValue(true, true);
+			Character->Multicast_ComboEnd();
+			Character->Multicast_SetCanHitReact(true);
+		}
+
+		Character->CanChangeSeekerGait = true;
+	}
+}
