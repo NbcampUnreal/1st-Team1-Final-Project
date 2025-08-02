c74e82b0fb64b8c15dc4072606805f67a98a4acc
raindrovvv <vnrs97@gmail.com>
2025-08-03 01:43:00 +0900

    [ 🩹 fix ] 무기 히트 사운드 및 VFX 위치 개선
    * 시커 타격 시 히트 사운드, VFX 추가 :: HitSeekerSoundEvent, HitSeekerVFX
    몬스터가 시커 플레이어를 때렸을 때도 SFX, VFX 출력됨!
    
    * 라인 트레이스를 통한 정확한 VFX 위치 계산 함수 추가 :: CalculateMoreAccurateHitPoint

--- Source/GAS/Public/Weapon/Equipable/GS_WeaponSword.h
+++ Source/GAS/Public/Weapon/Equipable/GS_WeaponSword.h
@@ -58,12 +58,18 @@ public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Audio")
 	UAkAudioEvent* HitStructureSoundEvent;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Audio")
+	UAkAudioEvent* HitSeekerSoundEvent;
+
 	// 히트 VFX 에셋들
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "VFX")
 	UNiagaraSystem* HitPawnVFX;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "VFX")
 	UNiagaraSystem* HitStructureVFX;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "VFX")
+	UNiagaraSystem* HitSeekerVFX;
 
 	UPROPERTY()
 	class AGS_Character* OwnerChar;
@@ -85,6 +91,9 @@ private:
 	virtual ESwordHitTargetType DetermineTargetType(AActor* OtherActor) const;
 	virtual void PlayHitSound(ESwordHitTargetType TargetType, const FHitResult& SweepResult);
 	virtual void PlayHitVFX(ESwordHitTargetType TargetType, const FHitResult& SweepResult);
+	
+	// 히트 포인트 계산
+	virtual FHitResult CalculateMoreAccurateHitPoint(AActor* OtherActor) const;
 
 	// 멀티캐스트 함수들
 	UFUNCTION(NetMulticast, Reliable)
