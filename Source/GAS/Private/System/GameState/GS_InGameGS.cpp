95773a20a4939ebffee18f4776563905639e33fd
Blanquette0315 <tjfwn0315@gmail.com>
2025-08-28 16:18:09 +0900

    [ 🩹 fix ] 방 숨김처리 문제 해결을 위한 로그 추가

--- Source/GAS/Private/System/GameState/GS_InGameGS.cpp
+++ Source/GAS/Private/System/GameState/GS_InGameGS.cpp
@@ -24,6 +24,7 @@ void AGS_InGameGS::SetDungeonData(int32 InTotalRoomCount)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("[방 숨김] 서버에서만 호출 bool값 변경 완료"));
 		TotalRoomCount = InTotalRoomCount;
+		UE_LOG(LogTemp, Warning, TEXT("[방 숨김] TotalRoomCount = %d, InTotalRoomCount = %d"), TotalRoomCount, InTotalRoomCount);
 
 		// 이 변수를 true로 설정하면, 잠시 후 모든 클라이언트에서 OnRep_DungeonDataReplicated가 호출됩니다.
 		bDungeonDataReady = true;
@@ -95,16 +96,23 @@ void AGS_InGameGS::OnRep_DungeonDataReplicated()
 	// bDungeonDataReady가 true라는 신호를 받으면 검증을 시작합니다.
 	if (bDungeonDataReady)
 	{
-	UE_LOG(LogTemp, Warning, TEXT("[방 숨김] 클라이언트에서 방 숨김 진행"));
+		UE_LOG(LogTemp, Warning, TEXT("[방 숨김] 클라이언트에서 방 숨김 진행"));
 		// Guardian 역할을 가진 플레이어의 클라이언트에서만 벽 숨김 처리를 위한 검증을 시작합니다.
 		if (AGS_RTSController* MyController = Cast<AGS_RTSController>(GetGameInstance()->GetFirstLocalPlayerController()))
 		{
 			UE_LOG(LogTemp, Warning, TEXT("[방 숨김] 컨트롤러가 RTS인 애들만 진행"));
-			AGS_PlayerState* PS = MyController->GetPlayerState<AGS_PlayerState>();
-			if (PS && PS->CurrentPlayerRole == EPlayerRole::PR_Guardian)
+			if (AGS_PlayerState* PS = MyController->GetPlayerState<AGS_PlayerState>())
 			{
-				UE_LOG(LogTemp, Warning, TEXT("[로딩] 서버 던전 방 생성 준비 완료, 클라 검증 중.."));
-				Client_VerifyRoomSpawning();
+				UE_LOG(LogTemp, Warning, TEXT("[로딩] PlayerState 찾음"));
+				if (PS->CurrentPlayerRole == EPlayerRole::PR_Guardian)
+				{
+					UE_LOG(LogTemp, Warning, TEXT("[로딩] 서버 던전 방 생성 준비 완료, 클라 검증 중.."));
+					Client_VerifyRoomSpawning();
+				}
+				else
+				{
+					UE_LOG(LogTemp, Warning, TEXT("[로딩] 플레이어 룰이 가디언이 아님. 방 숨김 처리 취소"));
+				}
 			}
 		}
 	}
