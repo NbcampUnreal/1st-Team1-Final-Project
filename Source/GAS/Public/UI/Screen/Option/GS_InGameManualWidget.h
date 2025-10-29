44d42433157548beccb14a1608a2a9a0729ad9bb
sehee-jj <seheesay127@gmail.com>
2025-10-29 15:21:51 +0900

    [ 🩹 fix ] 인게임 메뉴얼 레이아웃 변경

--- Source/GAS/Public/UI/Screen/Option/GS_InGameManualWidget.h
+++ Source/GAS/Public/UI/Screen/Option/GS_InGameManualWidget.h
@@ -4,8 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "UI/Data/GS_ManualRow.h"
 #include "GS_InGameManualWidget.generated.h"
 
+class UImage;
+class UVerticalBox;
+class UDataTable;
 class UCommonButtonBase;
 
 /**
@@ -16,12 +20,85 @@ class GAS_API UGS_InGameManualWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UGS_InGameManualWidget(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable, Category = "Manual")
+	void InitManual();
+
+	UFUNCTION(BlueprintCallable, Category = "Manual")
+	void GoToPage(uint8 PageIndex);
+
+	UFUNCTION(BlueprintCallable, Category = "Manual")
+	void GoToPrevPage();
+
+	UFUNCTION(BlueprintCallable, Category = "Manual")
+	void GoToNextPage();
+
+	UFUNCTION(BlueprintPure, Category = "Manual")
+	uint8 GetCurrPageIndex() const { return CurrPageIndex; }
+
+	UFUNCTION(BlueprintPure, Category = "Manual")
+	uint8 GetTotalPageCount() const { return ManualDataArray.Num(); }
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void InitButton(UCommonButtonBase* PageButton, const FText& ButtonText);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void ChangeButtonState(UCommonButtonBase* PageButton, bool bIsCurrPage);
+
 protected:
 	virtual void NativeConstruct() override;
 
 	UPROPERTY(meta = (BindWidget))
-	UCommonButtonBase* CloseButton;
+	UImage* ManualImage;
+
+	UPROPERTY(meta = (BindWidget))
+	UVerticalBox* PageButtonContainer;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonButtonBase* PrevButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonButtonBase* NextButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonButtonBase* ExitButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tutorial")
+	UDataTable* ManualImageTable;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Tutorial")
+	uint8 CurrPageIndex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tutorial")
+	TSubclassOf<UCommonButtonBase> PageButtonClass;
+
+	UPROPERTY()
+	TArray<UCommonButtonBase*> PageButtons;
+
+	UPROPERTY()
+	TArray<FManualImageRow> ManualDataArray;
+
+	void LoadManualData();
+
+	void CreatePageButtons();
+
+	void UpdateCurrPage();
+
+	void UpdatePageButtonStates();
+
+	void OnPageButtonClicked(uint8 PageIndex);
 
 	UFUNCTION()
-	void OnCloseButtonClicked();
+	void OnPrevButtonClicked();
+
+	UFUNCTION()
+	void OnNextButtonClicked();
+
+	UFUNCTION()
+	void OnExitButtonClicked();
+
+private:
+	void BindPageButtonEvents();
 };
