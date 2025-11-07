76e2c87359b93b2457b8d95225c974fae5aac243
sehee-jj <seheesay127@gmail.com>
2025-11-07 23:04:21 +0900

    [ 🐞 bugfix ] 언어 옵션에 드롭박스 적용안되는 버그 수정

--- Source/GAS/Private/UI/Screen/Option/GS_GeneralOptionUI.cpp
+++ Source/GAS/Private/UI/Screen/Option/GS_GeneralOptionUI.cpp
@@ -2,4 +2,20 @@
 
 
 #include "UI/Screen/Option/GS_GeneralOptionUI.h"
+#include "Internationalization/StringTable.h"
 
+void UGS_GeneralOptionUI::NativeConstruct()
+{
+	Super::NativeConstruct();
+	LoadedTable = nullptr;
+}
+
+FText UGS_GeneralOptionUI::GetLocalizedText(FString Key)
+{
+	if (LoadedTable)
+	{
+		FName TableId = LoadedTable->GetStringTableId();
+		return FText::FromStringTable(TableId, Key);
+	}
+	return FText::GetEmpty();
+}
