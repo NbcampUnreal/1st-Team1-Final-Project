a0d951a1cc2ba22b2eee0d570d9778ec0a51d592
raindrovvv <vnrs97@gmail.com>
2025-11-11 01:38:31 +0900

    [ 🩹 fix ] 찬 방어 함수 변경
    EnableAttackHit() : 로컬에서 즉시 실행되므로 네트워크 지연 없이 애니메이션 타이밍에 콜리전 활성화

--- Source/GAS/Private/Animation/Notifies/GS_AN_ShieldAttack.cpp
+++ Source/GAS/Private/Animation/Notifies/GS_AN_ShieldAttack.cpp
@@ -39,9 +39,8 @@ void UGS_AN_ShieldAttack::Notify(USkeletalMeshComponent* MeshComp, UAnimSequence
 	{
 		if (AGS_WeaponShield* Shield = Cast<AGS_WeaponShield>(Chan->GetWeaponByIndex(i)))
 		{
-			// 방패 공격 콜리전 활성화 (타이머는 내부에서 안전하게 관리됨)
-			// ServerEnableAttackHit()가 3초 안전 타이머를 자동으로 설정함
-			Shield->ServerEnableAttackHit();
+			// EnableAttackHit()가 내부적으로 안전 타이머를 관리함
+			Shield->EnableAttackHit();
 			break;
 		}
 	}
