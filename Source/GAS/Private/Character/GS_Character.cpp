d408829cbb9b70b97f4eeecafb3cabc6f6cb8732
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-11-11 12:54:03 +0900

    [ 📚comment ] 테스트용 로그 주석처리

--- Source/GAS/Private/Character/GS_Character.cpp
+++ Source/GAS/Private/Character/GS_Character.cpp
@@ -407,7 +407,7 @@ void AGS_Character::SetCharacterSpeed(float InRatio)
 {
 	if (InRatio >= 0.4f && this->GetDebuffComp()->IsDebuffActive(EDebuffType::Slow))
 	{
-		UE_LOG(LogTemp, Error, TEXT("Character Speed(제한됨) = %f"), CharacterSpeed);
+		//UE_LOG(LogTemp, Error, TEXT("Character Speed(제한됨) = %f"), CharacterSpeed);
 		return;
 	}
 
@@ -415,9 +415,9 @@ void AGS_Character::SetCharacterSpeed(float InRatio)
 	{
 		CharacterSpeed = DefaultCharacterSpeed * InRatio;
 		GetCharacterMovement()->MaxWalkSpeed = CharacterSpeed;
-		UE_LOG(LogTemp, Error, TEXT("Character Speed(변경됨) = %f"), CharacterSpeed);
-		UE_LOG(LogTemp, Warning, TEXT("SpeedCheck: Slow=%s"),
-			this->GetDebuffComp()->IsDebuffActive(EDebuffType::Slow) ? TEXT("True") : TEXT("False"));
+		//UE_LOG(LogTemp, Error, TEXT("Character Speed(변경됨) = %f"), CharacterSpeed);
+		//UE_LOG(LogTemp, Warning, TEXT("SpeedCheck: Slow=%s"),
+			//this->GetDebuffComp()->IsDebuffActive(EDebuffType::Slow) ? TEXT("True") : TEXT("False"));
 	}
 }
 
@@ -430,14 +430,14 @@ void AGS_Character::Server_SetCharacterSpeed_Implementation(float InRatio)
 {
 	if (InRatio >= 0.8f && this->GetDebuffComp()->IsDebuffActive(EDebuffType::Slow))
 	{
-		UE_LOG(LogTemp, Error, TEXT("Character Speed(제한됨) = %f"), CharacterSpeed);
+		//UE_LOG(LogTemp, Error, TEXT("Character Speed(제한됨) = %f"), CharacterSpeed);
 		return;
 	}
 
 	CharacterSpeed = DefaultCharacterSpeed * InRatio;
-	UE_LOG(LogTemp, Error, TEXT("Character Speed(변경됨) = %f"), CharacterSpeed);
-	UE_LOG(LogTemp, Warning, TEXT("SpeedCheck: Slow=%s"),
-		this->GetDebuffComp()->IsDebuffActive(EDebuffType::Slow) ? TEXT("True") : TEXT("False"));
+	//UE_LOG(LogTemp, Error, TEXT("Character Speed(변경됨) = %f"), CharacterSpeed);
+	//UE_LOG(LogTemp, Warning, TEXT("SpeedCheck: Slow=%s"),
+		//this->GetDebuffComp()->IsDebuffActive(EDebuffType::Slow) ? TEXT("True") : TEXT("False"));
 
 	if (HasAuthority())
 	{
